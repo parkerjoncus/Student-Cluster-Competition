ca16403c19ad5002ffcb1a8df67c819d71b5d01f
Parker Joncus <parkerj@me.com>
2018-07-18 09:58:43 -0500

    added different times and more plots
    changed time to be set and user has to change it by min hour and sec
    and program calculates in seconds. Added a couple of plots that look at
    a little after the initial flops since the first one is always much
    higher

--- plotFlops.R
+++ plotFlops.R
@@ -6,8 +6,13 @@ mydata<-mydata[,-1]
 mydata<-mydata[,-2]
 mydata<-mydata[,-3]
 mydata[,'time']<-0
-Time=13.37 #change the amount of time depending on what the output of the hpl is
-x=Time/length(mydata$Column)
+hour<-0
+min<-3
+sec<-16.061
+Time<-hour*3600+min*60+sec #change the amount of time depending on what the output of the hpl is
+x<-Time/length(mydata$Column)
 mydata$time<-seq(0,Time,x)[-1]
-ggplot(mydata, aes(x=Fraction, y=Gflops))+geom_point()+geom_line()
-ggplot(mydata, aes(x=time, y=Gflops))+geom_point()+geom_line()
+ggplot(mydata[-1,], aes(x=Fraction, y=Gflops))+geom_point()+geom_line()
+ggplot(mydata[-1,], aes(x=time, y=Gflops))+geom_point()+geom_line()
+ggplot(mydata[10:length(mydata$Column),], aes(x=time, y=Gflops))+geom_point()+geom_line()
+ggplot(mydata[10:length(mydata$Column),], aes(x=Fraction, y=Gflops))+geom_point()+geom_line()
